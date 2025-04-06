0dacc31a5cf4b40469367fb2e3dcc39776e40847
nandanchakraborty <nandancsebubt@gmail.com>
2025-04-06 23:40:47 +0600

    Student class

--- Student.java
+++ Student.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Student {
+    private String name;
+    private int [] marks;
+   Student(String name ,int []marks){
+       this.name = name;
+       this .marks = marks;
+
+   }
+
+ public  int getNumberOfMarks(){
+       return marks.length;
+  }
+  public int getTotalSumOfMark(){
+       int sum =0;
+       for(int mark:marks){
+           sum = sum+mark;
+
+       }
+       return sum;
+    }
 }
