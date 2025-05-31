b5782203fa93d6f9f69e398fe898c0d957305f14
nandanchakraborty <nandancsebubt@gmail.com>
2025-05-31 15:57:11 +0600

    a little bit update

---
+++ FanRunner.java
@@ -0,0 +1,12 @@
+public class FanRunner {
+    private String make;
+    private double radius;
+    private String color;
+    private boolean isOn;
+    private byte speed;
+
+
+    public static void main(String[] args) {
+
+    }
+}
