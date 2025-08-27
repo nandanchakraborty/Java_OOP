3c84b7e601858078a32dbabc83d9527d52d56430
nandanchakraborty <nandancsebubt@gmail.com>
2025-08-27 19:13:33 +0600

    calling returnable thread multiple time using list

--- Thread/MultipleCallableRunner.java
+++ Thread/MultipleCallableRunner.java
@@ -1,4 +1,22 @@
 package Thread;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class MultipleCallableRunner {
-}
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorservice = Executors.newFixedThreadPool(3); //this is a multiple thread executor
+
+        List<CallableTask> task = List.of(new CallableTask("Nandan"), new CallableTask("Pranta"),
+                new CallableTask("MUMU"));
+        List<Future<String>> results = executorservice.invokeAll(task);
+        for(Future<String> result: results){
+            System.out.println(result.get());
+        }
+
+
+        executorservice.shutdown();
+    }}
