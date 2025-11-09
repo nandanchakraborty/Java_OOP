56988b9584a588eeddc8a41ec8a262c24a6d3b2d
nandanchakraborty <nandancsebubt@gmail.com>
2025-11-09 21:46:17 +0600

    about anonymous class

--- JavaTips/Anonymous.java
+++ JavaTips/Anonymous.java
@@ -1,4 +1,25 @@
 package JavaTips;
-
-public class Anonymous {
+class Animal {
+    public void makeSound(){
+        System.out.println("animal sound");
+    }
 }
+public class Anonymous {
+    public static void main(String[] args){
+        Animal animal = new Animal(){
+            public void makeSound(){
+                System.out.println("woof woof");
+
+            }
+        };
+        animal.makeSound();
+    }
+}
+/*
+An anonymous class is a class without a name. It is created and used at the same time.
+
+You often use anonymous classes to override methods of an existing class or
+interface, without writing a separate class file.
+
+Here, we create an anonymous class that extends another class and overrides its method:
+ */
