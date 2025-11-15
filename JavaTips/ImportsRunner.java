ef509d725af1bfc2ccd8d89b1ba4c5507b79895e
nandanchakraborty <nandancsebubt@gmail.com>
2025-11-15 21:18:38 +0600

    Imports runner

--- JavaTips/ImportsRunner.java
+++ JavaTips/ImportsRunner.java
@@ -1,4 +1,10 @@
 package JavaTips;
-
+import static java.lang.System.out;
 public class ImportsRunner {
+    public static void main(String[]args) {
+
+        String s = "Nandan ";
+        out.println(s);
+    }
 }
+
